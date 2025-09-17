fe367f30515288c4f83a64982b2877060d62f139
sushant <sushant.khadka@ebpearls.com>
2025-09-17 09:56:42 +0545

    removed unuseable function

--- lib/extensions/captalize_extension.dart
+++ lib/extensions/captalize_extension.dart
@@ -3,11 +3,5 @@ extension CaptalizeExtension on String {
     return this[0].toUpperCase() + substring(1).toLowerCase();
   }
 
-  String ohho() {
-    return "ddd";
-  }
 
-  int checking() {
-    return 1;
-  }
 }
